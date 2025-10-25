c573dad6f600485f484164e0a87a0902e2c36a3f
Agustín Arturo Melian Su <142176619+aguamelsu@users.noreply.github.com>
2025-10-25 01:37:28 +0000

    Add app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import logging
+from flask import Flask, request, render_template
+import pickle
+import pandas as pd
+
+# Create app
+app = Flask(__name__)
+
+# Load pre-trained model
+with open('/workspaces/production-practice/models/iris_models.pkl', 'rb') as file:
+    artifact = pickle.load(file)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def prediction():
+    # Homework: validate each value with ratios 
+    if request.method == 'POST':
+        val1 = float(request.form['val1'])
+        val2 = float(request.form['val2'])
+        val3 = float(request.form['val3'])
+        val4 = float(request.form['val4'])
+
+        df_predict = pd.DataFrame([[val1, val2, val3, val4]], 
+                                  columns=artifact['predictors'])
+        
+        result = artifact['model'].predict(df_predict)
+        predicted_label = artifact['target_encoder'].inverse_transform(result)
+    
+    else:
+        predicted_label = None
+    
+    return render_template('index.html', prediction=predicted_label)
